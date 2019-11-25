3929eba82a0ac69bb61524c970957bd4131e62d8
fin <nayanzin.alexander@gmail.com>
2019-11-25 19:29:57 +0200

    Initial commit

---
+++ src/main/java/com/nayanzin/micrometerprometeusgrafana/MicrometerPrometeusGrafanaApplication.java
@@ -0,0 +1,13 @@
+package com.nayanzin.micrometerprometeusgrafana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrometerPrometeusGrafanaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicrometerPrometeusGrafanaApplication.class, args);
+    }
+
+}
